bd8282eb3b9deb4bf5103ffa953c4da0e9146d53
Elias Valter <evalter1612@gmail.com>
2022-07-15 23:11:56 +0200

    tech ana

--- analysis.py
+++ analysis.py
@@ -1,12 +1,18 @@
+from this import d
+import numpy as np
 import pandas as pd
-from pandas_datareader import data
+import talib as ta
+import pandas_datareader as web
 import matplotlib.pyplot as plt
+import datetime as dt
+
 
 START_DATE = '2021/1/1'
 END_DATE = '2022/1/1'
 
-meta = data.DataReader('META', 'yahoo', START_DATE, END_DATE)
-apple = data.DataReader('AAPL', 'yahoo', START_DATE, END_DATE)
-amzn = data.DataReader('AMZN', 'yahoo', START_DATE, END_DATE)
-nflx = data.DataReader('NFLX', 'yahoo', START_DATE, END_DATE)
-goog = data.DataReader('GOOG', 'yahoo', START_DATE, END_DATE)
+data = web.DataReader('AAPL', 'yahoo', START_DATE, END_DATE)
+
+#Simple Moving Average
+data['thisIsAColumnName'] = ta.SMA(data['Close'], 100)
+
+
---
+++ yep.py
@@ -0,0 +1,5 @@
+import numpy as np
+import talib as ta
+import matplotlib.pyplot as plt
+import datetime as dt
+
